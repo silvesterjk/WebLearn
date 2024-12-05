6ec082640b544c1c37e144279c560d4f11e8f94d
silvesterjk <silvesterjkennedy@gmail.com>
2024-12-05 20:03:39 +0530

    5/12 - 3rd

---
+++ front-end/javascript/syntax/objects/constructor-func.js
@@ -0,0 +1,6 @@
+// Object literal syntax creates a single object, but factory functions offer a reusable way to generate objects 
+// with the same state and behavior (properties and methods).
+// However, constructor functions are more commonly used for this purpose.
+// Constructor functions initialize an object's state and are named using PascalCase.
+// The 'this' keyword is used within the constructor to define properties and methods for the object.
+
