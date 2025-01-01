ffd00bbfe21f61d0d6fdb53ee5dd0d6e7cefce4c
silvesterjk <silvesterjkennedy@gmail.com>
2025-01-01 21:07:46 +0530

    1/1 - 2nd

--- front-end/javascript/syntax/3objects/9-cloning-an-object-and-grabagecoll.js
+++ front-end/javascript/syntax/3objects/9-cloning-an-object-and-grabagecoll.js
@@ -24,3 +24,8 @@ Object.assign(b, a);
 b = { ...a };
 
 // Creating two independent objects
+
+// In programming languages such as C or C++,
+// when you create an object you have to explicitly allocate and deallocate memory.
+// But in programming languages such as JavaScript, C#, Java, or Python
+// the languages have garbage collection to manage memory for us.
