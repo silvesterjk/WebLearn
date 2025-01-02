1cc2de7046848bf965ef6b9457d55e11f3708b79
silvesterjk <silvesterjkennedy@gmail.com>
2025-01-02 22:22:56 +0530

    2/1 - 5th

--- front-end/javascript/syntax/10-filtering-and-mapping.js
+++ front-end/javascript/syntax/10-filtering-and-mapping.js
@@ -25,3 +25,29 @@ const developers = employees.filter(employee => employee.role === 'Developer');
 console.log(developers);
 
 
+// The .map() method is a cornerstone of array manipulation.
+// It's a powerful way to process and transform array elements.
+
+// It operates on each element of an array,
+// applying a function that you specify, and returns a new array composed of the results.
+
+// So it allows you to transform data without altering the original array.
+
+const numbers2 = [2, 4, 6, 8, 10];
+const squaredNumbers = numbers2.map(num => num * num);
+console.log('squaredNumbers ', squaredNumbers);
+
+const characters = ['a', 'b', 'c', 'd'];
+const upperCaseCharacters = characters.map(char => char.toUpperCase());
+console.log('upperCaseCharacters', upperCaseCharacters);
+
+const employees2 = [
+    { id: 1, name: 'Alice', email: 'AliCe@gmail.com' },
+    { id: 2, name: 'Steven', email: 'STeVen@gmail.com' },
+    { id: 3, name: 'Joe', email: 'Joe@gmail.com' },
+];
+
+const updatedEmployees = employees2.map(employee => ({
+    ...employee,
+    email: email.toLowerCase()
+}));
